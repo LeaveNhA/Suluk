3de38f9336b33802a4ebaccf24c9a5c8eebfd71e
LeaveNhA <seckin.kukrer@hotmail.com>
2017-11-14 23:36:14 +0300

    untrack resources file

--- resources/public/js/compiled/out/suluk/requests/core.cljs
+++
@@ -1,8 +0,0 @@
-(ns suluk.requests.core
-  (:require [suluk.requests.type :as t]
-            [suluk.requests.get :as g]
-            [suluk.requests.post :as p]))
-
-(def post p/post)
-(def get g/get)
-(def post-json p/post-json)
