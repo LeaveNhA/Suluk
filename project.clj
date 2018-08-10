a083f841ffbdc321c56225ed5bfd373fb0d4b88e
LeaveNhA <seckinkukrer@aof.anadolu.edu.tr>
2018-08-10 17:08:27 +0300

    Update for Clojars! 😏

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.scknkkrer/suluk "0.0.14"
+(defproject org.clojars.scknkkrer/suluk "0.0.16"
   :description "Fetch API wrapper for clojurescript."
   :url "https://github.com/LeaveNhA/suluk"
   :license {:name "Eclipse Public License"
